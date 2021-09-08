1246fe0e0f0d5b224cf61199922e53e41e618a35
yucco <59766001+yucco3@users.noreply.github.com>
2021-09-08 21:56:05 +0900

    更新：ショートテキスト入力APP：文字数チェック追加

--- textEditor140/js/editor.js
+++ textEditor140/js/editor.js
@@ -2,14 +2,24 @@
 function submit(){
     console.log(INPUT_OBJECT.value);
     let input_text = INPUT_OBJECT.value;
+    if(input_text.length > 140){return}
     let old_text = DISPLAY_TEXT_OBJECT.value;
     let new_text = old_text + "\n" + input_text;
     DISPLAY_TEXT_OBJECT.value = new_text;
     INPUT_OBJECT.value = "";
 }
 
+function check_input(event){
+    let len = INPUT_OBJECT.value.length;
+    COUNT_OBJECT.innerHTML = len;
+    if(len <= 140){COUNT_OBJECT.className = "count_nomal"}
+    if(len > 140){COUNT_OBJECT.className = "count_over"}
+}
+
 
 let DISPLAY_TEXT_OBJECT = document.getElementById("display_text");
 let INPUT_OBJECT = document.getElementById("input_text");
 let SUBMIT_BUTTON = document.getElementById("submit");
+let COUNT_OBJECT = document.getElementById("count");
 
+INPUT_OBJECT.addEventListener("input", check_input)
