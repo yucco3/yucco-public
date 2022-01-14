37a2868aa1fba5dd41ad8dc8284e9d278c63a1f8
yucco <59766001+yucco3@users.noreply.github.com>
2022-01-14 20:31:12 +0900

    更新：ショートテキスト入力APP：便利機能を色々追加
    コピーした後のメッセージをインライン化したり、ふっかつのじゅもんを追加したり

--- text_editor140/js/editor.js
+++ text_editor140/js/editor.js
@@ -1,4 +1,3 @@
-
 function submit(){
     console.log(INPUT_OBJECT.value);
     let input_text = INPUT_OBJECT.value;
@@ -9,9 +8,14 @@ function submit(){
     if(old_text != ""){connect = "\n"}
     let new_text = old_text + connect + input_text
 
+    SUBMIT_COUNT = SUBMIT_COUNT + 1;
+    let total_count = new_text.length;
+
     DISPLAY_TEXT_OBJECT.value = new_text;
     INPUT_OBJECT.value = "";
     COUNT_OBJECT.innerHTML = 0;
+    SUBMIT_COUNT_OBJECT.innerHTML = SUBMIT_COUNT;
+    TOTAL_COUNT_OBJECT.innerHTML = total_count;
 }
 
 
@@ -25,7 +29,12 @@ function checkInput(event){
 function clearAll(){
     console.log("clear")
     if(!window.confirm("入力内容を消去します")){return}
-    DISPLAY_TEXT_OBJECT.value = ""
+    BACKUP = DISPLAY_TEXT_OBJECT.value;
+
+    DISPLAY_TEXT_OBJECT.value = "";
+    SUBMIT_COUNT = 0;
+    SUBMIT_COUNT_OBJECT.innerHTML = SUBMIT_COUNT;
+    TOTAL_COUNT_OBJECT.innerHTML = 0;
 }
 
 function copyText(){
@@ -33,14 +42,31 @@ function copyText(){
     let text = DISPLAY_TEXT_OBJECT.value;
     if(!navigator.clipboard){alert("このブラウザでは、このボタンからはコピーできません。文字列を選択して、直接コピーしてください。")}
     navigator.clipboard.writeText(text).then(
-        ()=>{alert("クリップボードにコピーしました")},
+        displayCopyAlert,
         ()=>{alert("コピーに失敗しました。文字列を選択して、直接コピーしてください。")}
         );
+}
+
+function displayCopyAlert(){
+    document.getElementById("copy_message").innerHTML = "クリップボードにコピーしました";
+    setTimeout(() => {
+        document.getElementById("copy_message").innerHTML = "";
+    }, 3000);
+}
+
+function restore(){
+    alert("ふっかつのじゅもん！");
+    document.getElementById("restore").innerHTML = BACKUP.replace("\n","<br />");
 }
 
 let DISPLAY_TEXT_OBJECT = document.getElementById("display_text");
 let INPUT_OBJECT = document.getElementById("input_text");
 let SUBMIT_BUTTON = document.getElementById("submit");
 let COUNT_OBJECT = document.getElementById("count");
+let SUBMIT_COUNT_OBJECT = document.getElementById("submit_count");
+let TOTAL_COUNT_OBJECT = document.getElementById("total_count");
+
+let SUBMIT_COUNT = 0;
+let BACKUP = "";
 
 INPUT_OBJECT.addEventListener("input", checkInput)
